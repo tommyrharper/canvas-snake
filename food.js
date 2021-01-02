f76c4995e6151a3aa4886b5130898248a75b7ad6
tommyrharper <thomasrobertharper@gmail.com>
2021-01-02 14:03:10 +0000

    Fix food.js draw function

--- food.js
+++ food.js
@@ -8,6 +8,6 @@ export function draw(gameBoard) {
     const foodElement = document.createElement('div');
     foodElement.style.gridRowStart = food.y;
     foodElement.style.gridColumnStart = food.x;
-    foodElement.classList.add('snake');
+    foodElement.classList.add('food');
     gameBoard.appendChild(foodElement);
 }
