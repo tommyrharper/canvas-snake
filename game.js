3198dda5732c2033a6493745317de8faa788feaf
tommyrharper <thomasrobertharper@gmail.com>
2021-01-02 13:38:13 +0000

    import snake.js into game.js

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js';
 let lastRenderTime = 0;
-const SNAKE_SPEED = 2;
 
 function main(currentTime) {
   window.requestAnimationFrame(main);
@@ -7,6 +7,17 @@ function main(currentTime) {
   if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
   console.log('render');
   lastRenderTime = currentTime;
+
+  update();
+  draw();
 }
 
 window.requestAnimationFrame(main);
+
+function update() {
+  updateSnake();
+}
+
+function draw() {
+  drawSnake();
+}
